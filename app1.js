e86bb26357bc611bfa5fbed53089252f780b1d3a
anishpdm <anish.vilayil.s2gmail.com>
2020-04-03 15:23:46 +0530

    mongo db added

--- app1.js
+++ app1.js
@@ -1,32 +1,55 @@
-var express = require("express");
+var express = require('express');
 var bodyParser = require('body-parser');
+var Mongoose = require('mongoose');
 
 
 var app = express();
-
-
 app.use(bodyParser.urlencoded({
     extended: false
 }))
 
 
+const studentSchema = new Mongoose.Schema({
+    name: String,
+    roll: Number,
+    adminNo: Number,
+    clg: String
+
+});
+
+var studentModel = Mongoose.model('students', studentSchema);
+
+Mongoose.connect("mongodb+srv://anishsnair:hello12345@cluster0-rqfpy.mongodb.net/test?retryWrites=true&w=majority");
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
 
-    res.json({
-        "result": "test"
-    });
-
-});
-
-app.post('/read', (req, res) => {
-
-    var name = req.body.getname;
-    var roll = req.body.getroll;
-    console.log(req);
-    res.send(req.body);
+    res.send("hai..");
 });
 
 
+app.post('/reg', (req, res) => {
+
+
+    try {
+
+        var studentData = new studentModel(req.body);
+        var result = await studentData.save();
+
+        res.json(result);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+
+
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("server started");
